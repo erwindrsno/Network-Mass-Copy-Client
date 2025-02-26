8f0ddb11be1a877bf98b39d0cecef004f1ca8dfd
Erwin Darsono <erwindarsono08@gmail.com>
2025-02-26 21:55:18 +0800

    conf buil-gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("org.java-websocket:Java-WebSocket:1.6.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
 
