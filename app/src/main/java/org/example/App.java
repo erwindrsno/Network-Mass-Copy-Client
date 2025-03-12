6785dc146480be9fc4786b1b2ae742adf395881b
Erwin Darsono <96133919+erwindrsno@users.noreply.github.com>
2025-03-12 18:34:03 +0800

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -14,7 +14,7 @@ public class App {
         System.out.println("Connecting to localhost");
         // System.out.println("Connecting to 10.100.70.60:8887");
         // WebSocketClient client = new Client(new URI("ws://192.168.0.102:8887"));
-        WebSocketClient client = new Client(new URI("ws://127.0.0.1:8887"));
+        WebSocketClient client = new Client(new URI("ws://10.100.70.60:8887"));
         client.connect();
     }
 }
