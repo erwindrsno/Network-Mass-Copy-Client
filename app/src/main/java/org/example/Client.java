3647f285f8a066f25bf6cac480d79c91c97e9275
Erwin Darsono <erwindarsono08@gmail.com>
2025-04-18 17:55:48 +0800

    fix memory bug

--- app/src/main/java/org/example/Client.java
+++ app/src/main/java/org/example/Client.java
@@ -29,139 +29,154 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.hash.Hashing;
 
 public class Client extends WebSocketClient {
-    Path toBeReceived = Paths.get("toBeReceived");
-    FileOutputStream fos;
-    Logger logger = LoggerFactory.getLogger(Client.class);
+  Path toBeReceived = Paths.get("toBeReceived");
+  FileOutputStream fos;
+  Logger logger = LoggerFactory.getLogger(Client.class);
 
-    boolean readyToReceiveFile = false;
+  boolean readyToReceiveFile = false;
 
-    byte[] fileBytes; // complete file in bytes
-    int currIdx = 0;
-    long fileSize;
-    long chunkSize;
+  byte[] fileBytes; // complete file in bytes
+  int currIdx = 0;
+  long fileSize;
+  long chunkSize;
 
-    FileMetadata fileMetadata;
+  FileMetadata fileMetadata;
 
-    public Client(URI serverUri, Draft draft) {
-        super(serverUri, draft);
-    }
+  public Client(URI serverUri, Draft draft) {
+    super(serverUri, draft);
+  }
 
-    public Client(URI serverURI) {
-        super(serverURI);
-    }
+  public Client(URI serverURI) {
+    super(serverURI);
+  }
 
-    @Override
-    public void onOpen(ServerHandshake handshakedata) {
-        logger.info("Connected to server : " + handshakedata.getHttpStatusMessage());
-    }
+  @Override
+  public void onOpen(ServerHandshake handshakedata) {
+    logger.info("Connected to server : " + handshakedata.getHttpStatusMessage());
+  }
 
-    @Override
-    public void onMessage(String message) {
-        if (message.equals("PING")) {
-            send("PONG");
-        } else if (message.startsWith("FILE-METADATA~")) {
-            try {
-                fos = new FileOutputStream(toBeReceived.toFile());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            String json = message.substring(message.indexOf('~') + 1);
-            ObjectMapper mapper = new ObjectMapper();
-            try {
-                this.fileMetadata = mapper.readValue(json, FileMetadata.class);
-                this.fileSize = this.fileMetadata.getFileSize();
-                // this.chunkSize = this.fileMetadata.getChunkSize();
-                this.fileBytes = new byte[(int) this.fileSize];
-                this.readyToReceiveFile = true;
-                logger.info("RECEIVED META DATA");
-                send("READY-FILE~");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+  @Override
+  public void onMessage(String message) {
+    if (message.equals("PING")) {
+      send("PONG");
+    } else if (message.startsWith("FILE-METADATA~")) {
+      try {
+        fos = new FileOutputStream(toBeReceived.toFile());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      String json = message.substring(message.indexOf('~') + 1);
+      ObjectMapper mapper = new ObjectMapper();
+      try {
+        this.fileMetadata = mapper.readValue(json, FileMetadata.class);
+        this.fileSize = this.fileMetadata.getFileSize();
+        // this.chunkSize = this.fileMetadata.getChunkSize();
+        this.fileBytes = new byte[(int) this.fileSize];
+        this.readyToReceiveFile = true;
+        logger.info("RECEIVED META DATA");
+        send("READY-FILE~");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else if (message.startsWith("DEL|")) {
+      String toDeleteFileName = message.substring(message.indexOf('|') + 1);
+      Path toDeletePath = Paths.get(toDeleteFileName);
+
+      try {
+        boolean isDeleted = Files.deleteIfExists(toDeletePath);
+        if (isDeleted) {
+          logger.info("File is deleted.");
+        } else {
+          logger.info("File is not EXIST");
         }
+      } catch (Exception e) {
+        logger.error(e.getMessage());
+      }
     }
+  }
 
-    @Override
-    public void onMessage(ByteBuffer buffer) {
-        if (this.readyToReceiveFile) {
-            try {
-                byte[] data = new byte[buffer.remaining()];
+  @Override
+  public void onMessage(ByteBuffer buffer) {
+    if (this.readyToReceiveFile) {
+      try {
+        byte[] data = new byte[buffer.remaining()];
 
-                buffer.get(data);
+        buffer.get(data);
 
-                System.arraycopy(data, 0, fileBytes, currIdx, data.length);
-                this.currIdx += data.length;
+        System.arraycopy(data, 0, fileBytes, currIdx, data.length);
+        this.currIdx += data.length;
 
-                if (this.currIdx == fileBytes.length) {
-                    fos.write(fileBytes);
-                    fos.close();
+        fos.write(fileBytes);
 
-                    this.readyToReceiveFile = false;
-                    this.currIdx = 0;
+        if (this.currIdx == fileBytes.length) {
+          fos.close();
 
-                    validateFileAndHandleAcl();
+          this.readyToReceiveFile = false;
+          this.currIdx = 0;
 
-                    send("FINISH-FILE~");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+          validateFileAndHandleAcl();
+
+          send("FINISH-FILE~");
         }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-        System.out.println("closed with exit code " + code + " additional info: " + reason);
+  @Override
+  public void onClose(int code, String reason, boolean remote) {
+    System.out.println("closed with exit code " + code + " additional info: " + reason);
+  }
+
+  @Override
+  public void onError(Exception ex) {
+    System.err.println("an error occurred:" + ex);
+  }
+
+  public void validateFileAndHandleAcl() {
+    try {
+      Path filePath = Paths.get(this.fileMetadata.getFileName());
+      String user = this.fileMetadata.getUser();
+      String signature = this.fileMetadata.getSignature();
+
+      Set<AclEntryPermission> permissions = this.fileMetadata.getAclEntry();
+
+      String hashedClient = Hashing.sha256().hashBytes(Files.readAllBytes(toBeReceived)).toString();
+
+      if (hashedClient.equals(signature)) {
+        logger.info("FILE VERIFIED");
+      } else {
+        toBeReceived.toFile().delete();
+        logger.info("FILE CORRUPTED");
+        return;
+      }
+      // file rename
+      // sourcePath is created beforehand
+      Files.move(toBeReceived, filePath, StandardCopyOption.REPLACE_EXISTING);
+
+      // UserPrincipal userPrincipal =
+      // filePath.getFileSystem().getUserPrincipalLookupService()
+      // .lookupPrincipalByName(user);
+
+      // AclFileAttributeView aclView = Files.getFileAttributeView(filePath,
+      // AclFileAttributeView.class);
+
+      // AclEntry aclEntry = AclEntry.newBuilder()
+      // .setType(AclEntryType.ALLOW)
+      // .setPrincipal(userPrincipal)
+      // .setFlags(AclEntryFlag.DIRECTORY_INHERIT, AclEntryFlag.FILE_INHERIT)
+      // .setPermissions(permissions)
+      // .build();
+
+      // List<AclEntry> acl = aclView.getAcl();
+      // acl.add(0, aclEntry);
+      // aclView.setAcl(acl);
+      logger.info("File received safely, phew");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    @Override
-    public void onError(Exception ex) {
-        System.err.println("an error occurred:" + ex);
-    }
-
-    public void validateFileAndHandleAcl() {
-        try {
-            Path filePath = Paths.get(this.fileMetadata.getFileName());
-            String user = this.fileMetadata.getUser();
-            String signature = this.fileMetadata.getSignature();
-
-            Set<AclEntryPermission> permissions = this.fileMetadata.getAclEntry();
-
-            String hashedClient = Hashing.sha256().hashBytes(fileBytes).toString();
-
-            if (hashedClient.equals(signature)) {
-                logger.info("FILE VERIFIED");
-            } else {
-                toBeReceived.toFile().delete();
-                logger.info("FILE CORRUPTED");
-                return;
-            }
-            // file rename
-            // sourcePath is created beforehand
-            Files.move(toBeReceived, filePath, StandardCopyOption.REPLACE_EXISTING);
-
-            // UserPrincipal userPrincipal =
-            // filePath.getFileSystem().getUserPrincipalLookupService()
-            // .lookupPrincipalByName(user);
-
-            // AclFileAttributeView aclView = Files.getFileAttributeView(filePath,
-            // AclFileAttributeView.class);
-
-            // AclEntry aclEntry = AclEntry.newBuilder()
-            // .setType(AclEntryType.ALLOW)
-            // .setPrincipal(userPrincipal)
-            // .setFlags(AclEntryFlag.DIRECTORY_INHERIT, AclEntryFlag.FILE_INHERIT)
-            // .setPermissions(permissions)
-            // .build();
-
-            // List<AclEntry> acl = aclView.getAcl();
-            // acl.add(0, aclEntry);
-            // aclView.setAcl(acl);
-            logger.info("File received safely, phew");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
 
 // Catatan Utama
