b7cbe9a70cb78cb692f179c96d176769548af68c
Erwin Darsono <erwindarsono08@gmail.com>
2025-05-12 21:23:04 +0800

    handle byte

--- app/src/main/java/org/example/Client.java
+++ app/src/main/java/org/example/Client.java
@@ -130,55 +130,65 @@ public class Client extends WebSocketClient {
 
   @Override
   public void onMessage(ByteBuffer buffer) {
-    if (!this.isFinished) {
-      try {
-        byte[] data = buffer.array();
-        this.listOfChunkMaps.get(this.receiveFileCounter).put(this.receiveChunkCounter, data);
-        this.receiveChunkCounter++;
-        if (this.listOfChunkMaps.get(this.receiveFileCounter).size() == this.listFileMetadata
-            .get(this.receiveFileCounter)
-            .getChunkCount()) {
-          logger.info("File transfer successfully!");
-
-          for (int i = 0; i < this.listOfChunkMaps.get(this.receiveFileCounter).size(); i++) {
-            this.listFos.get(this.receiveFileCounter).write(this.listOfChunkMaps.get(this.receiveFileCounter).get(i));
-          }
-          this.listFos.get(receiveFileCounter).close();
-
-          String hashedClient = Hashing.sha256()
-              .hashBytes(Files.readAllBytes(this.listPath.get(this.receiveFileCounter)))
-              .toString();
-
-          if (hashedClient.equals(this.listFileMetadata.get(this.receiveFileCounter).getSignature())) {
-            logger.info("FILE VERIFIED");
-          } else {
-            this.listPath.get(this.receiveFileCounter).toFile().delete();
-            logger.info("FILE CORRUPTED");
-          }
-          Path newname = Paths
-              .get("files/" + this.title + "/" + this.listFileMetadata.get(this.receiveFileCounter).getFileName());
-          logger.info(this.listFileMetadata.get(this.receiveFileCounter).getFileName());
-          Files.move(this.listPath.get(this.receiveFileCounter), newname, StandardCopyOption.REPLACE_EXISTING);
-
-          this.receiveChunkCounter = 0;
-          this.receiveFileCounter++;
-          if (this.receiveFileCounter < this.listOfChunkMaps.size()) {
-            send("file~" + this.receiveFileCounter + "CHUNK-ID~" + this.receiveChunkCounter);
-          } else if (this.receiveFileCounter == this.listOfChunkMaps.size() - 1) {
-            logger.info("ALL FILES RECEVIED.");
-            this.isFinished = true;
-            // this.clearAllListAndMap();
-          }
-        } else {
-          if (this.receiveFileCounter < this.listOfChunkMaps.size()) {
-            send("file~" + this.receiveFileCounter + "CHUNK-ID~" + this.receiveChunkCounter);
-            logger.info("Receiving...");
-          }
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
+    this.serverHandler.handleByte(buffer);
+    // if (!this.isFinished) {
+    // try {
+    // byte[] data = buffer.array();
+    // this.listOfChunkMaps.get(this.receiveFileCounter).put(this.receiveChunkCounter,
+    // data);
+    // this.receiveChunkCounter++;
+    // if (this.listOfChunkMaps.get(this.receiveFileCounter).size() ==
+    // this.listFileMetadata
+    // .get(this.receiveFileCounter)
+    // .getChunkCount()) {
+    // logger.info("File transfer successfully!");
+    //
+    // for (int i = 0; i < this.listOfChunkMaps.get(this.receiveFileCounter).size();
+    // i++) {
+    // this.listFos.get(this.receiveFileCounter).write(this.listOfChunkMaps.get(this.receiveFileCounter).get(i));
+    // }
+    // this.listFos.get(receiveFileCounter).close();
+    //
+    // String hashedClient = Hashing.sha256()
+    // .hashBytes(Files.readAllBytes(this.listPath.get(this.receiveFileCounter)))
+    // .toString();
+    //
+    // if
+    // (hashedClient.equals(this.listFileMetadata.get(this.receiveFileCounter).getSignature()))
+    // {
+    // logger.info("FILE VERIFIED");
+    // } else {
+    // this.listPath.get(this.receiveFileCounter).toFile().delete();
+    // logger.info("FILE CORRUPTED");
+    // }
+    // Path newname = Paths
+    // .get("files/" + this.title + "/" +
+    // this.listFileMetadata.get(this.receiveFileCounter).getFileName());
+    // logger.info(this.listFileMetadata.get(this.receiveFileCounter).getFileName());
+    // Files.move(this.listPath.get(this.receiveFileCounter), newname,
+    // StandardCopyOption.REPLACE_EXISTING);
+    //
+    // this.receiveChunkCounter = 0;
+    // this.receiveFileCounter++;
+    // if (this.receiveFileCounter < this.listOfChunkMaps.size()) {
+    // send("file~" + this.receiveFileCounter + "CHUNK-ID~" +
+    // this.receiveChunkCounter);
+    // } else if (this.receiveFileCounter == this.listOfChunkMaps.size() - 1) {
+    // logger.info("ALL FILES RECEVIED.");
+    // this.isFinished = true;
+    // // this.clearAllListAndMap();
+    // }
+    // } else {
+    // if (this.receiveFileCounter < this.listOfChunkMaps.size()) {
+    // send("file~" + this.receiveFileCounter + "CHUNK-ID~" +
+    // this.receiveChunkCounter);
+    // logger.info("Receiving...");
+    // }
+    // }
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    // }
   }
 
   @Override
