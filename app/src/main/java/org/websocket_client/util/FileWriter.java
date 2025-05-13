6e593ca54288bf4e165420328ad9a01571647c53
Erwin Darsono <erwindarsono08@gmail.com>
2025-05-13 21:28:44 +0800

    trycopying 8

--- app/src/main/java/org/websocket_client/util/FileWriter.java
+++ app/src/main/java/org/websocket_client/util/FileWriter.java
@@ -1,5 +1,8 @@
 package org.websocket_client.util;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.websocket_client.Client;
@@ -16,7 +19,16 @@ public class FileWriter {
   public void writeFile(FileChunkMetadata fcm, FileAccessInfo fai) {
     this.logger.info("fcm filename: " + fcm.getFilename());
     this.logger.info("fai pathname: " + fai.getPath());
-    // Path path = Path.of("D:\\Ujian\\");
+    try {
+      Path path = Path.of(fai.getPath());
+      Files.createDirectories(path.getParent());
+      for (long i = 0; i <= fcm.getChunkCount(); i++) {
+        Files.write(path, fcm.getMapOfChunks().get(i));
+      }
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
+    }
+
     // for (int i = 0; i < fcm.getMapOfChunks().size(); i++) {
     //
     // }
