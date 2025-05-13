aef7414c712fe8759d388ebe6afd048eaba6c7a8
Erwin Darsono <erwindarsono08@gmail.com>
2025-05-13 20:22:22 +0800

    try copying4

--- app/src/main/java/org/example/FileWriter.java
+++
@@ -1,5 +0,0 @@
-package org.websocket_client;
-
-public class FileWriter {
-
-}
--- app/src/main/java/org/example/MessageHandlerStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.websocket_client;
-
-import java.nio.ByteBuffer;
-
-public interface MessageHandlerStrategy {
-  void handleString(String message);
-
-  void handleByte(ByteBuffer buffer);
-}
