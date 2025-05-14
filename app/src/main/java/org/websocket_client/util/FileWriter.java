b4b399e82abc41fec05b451df255585bd5b9d05e
Erwin Darsono <erwindarsono08@gmail.com>
2025-05-14 18:42:29 +0800

    notif after receive1

--- app/src/main/java/org/websocket_client/util/FileWriter.java
+++ app/src/main/java/org/websocket_client/util/FileWriter.java
@@ -22,25 +22,15 @@ public class FileWriter {
   }
 
   public void writeFile(FileChunkMetadata fcm, FileAccessInfo fai) {
-    this.logger.info("fcm filename: " + fcm.getFilename());
-    this.logger.info("fai pathname: " + fai.getPath());
     try {
       Path path = Path.of(fai.getPath());
       Files.createDirectories(path.getParent());
       for (long i = 0; i < fcm.getChunkCount(); i++) {
         Files.write(path, fcm.getMapOfChunks().get(i));
       }
-      this.client.send("client/status/ok");
+      this.client.send("client/" + "ok/" + fcm.getFilename());
     } catch (Exception e) {
       logger.error(e.getMessage(), e);
     }
-
-    // for (int i = 0; i < fcm.getMapOfChunks().size(); i++) {
-    //
-    // }
-    // for (int i = 0; i < this.listOfChunkMaps.get(this.receiveFileCounter).size();
-    // i++) {
-    // this.listFos.get(this.receiveFileCounter).write(this.listOfChunkMaps.get(this.receiveFileCounter).get(i));
-    // }
   }
 }
