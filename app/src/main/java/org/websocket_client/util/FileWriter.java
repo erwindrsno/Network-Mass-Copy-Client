8823bd3acaadfc86dc62613123bc4df9b75fccb0
Erwin Darsono <erwindarsono08@gmail.com>
2025-05-16 01:10:32 +0700

    big progress

--- app/src/main/java/org/websocket_client/util/FileWriter.java
+++ app/src/main/java/org/websocket_client/util/FileWriter.java
@@ -2,6 +2,7 @@ package org.websocket_client.util;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,8 +26,15 @@ public class FileWriter {
     try {
       Path path = Path.of(fai.getPath());
       Files.createDirectories(path.getParent());
+
+      Files.deleteIfExists(path);
+
+      this.logger.info("the chunk count is: " + fcm.getChunkCount());
       for (long i = 0; i < fcm.getChunkCount(); i++) {
-        Files.write(path, fcm.getMapOfChunks().get(i));
+        if(i == fcm.getChunkCount() - 1){
+          this.logger.info("The file successfully written!");
+        }
+        Files.write(path, fcm.getMapOfChunks().get(i), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
       }
       this.client.send("client/" + "ok/" + fai.getId());
     } catch (Exception e) {
