2440faa5868b0561840e359a3c6f552f04ee3b45
Erwin Darsono <erwindarsono08@gmail.com>
2025-06-02 19:13:58 +0800

    merging

--- src/main/java/org/websocket_client/handler/MessageHandlerStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.websocket_client.handler;
-
-import java.nio.ByteBuffer;
-
-public interface MessageHandlerStrategy {
-  void handleString(String message);
-
-  void handleByte(ByteBuffer buffer);
-}
