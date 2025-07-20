93e834fa9039abf270a067f233e3e9c82e35e728
Erwin Darsono <erwindarsono08@gmail.com>
2025-07-21 00:07:43 +0800

    remove unnecessary variables

--- app/src/main/java/org/websocket_client/Client.java
+++ app/src/main/java/org/websocket_client/Client.java
@@ -1,14 +1,7 @@
 package org.websocket_client;
 
-import java.io.FileOutputStream;
 import java.net.URI;
 import java.nio.ByteBuffer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft;
@@ -16,32 +9,10 @@ import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.*;
 import org.websocket_client.handler.ServerHandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 
 public class Client extends WebSocketClient {
-  Path toBeReceived = Paths.get("toBeReceived");
-  FileOutputStream fos;
   Logger logger = LoggerFactory.getLogger(Client.class);
-
-  boolean readyToReceiveFile = false;
-
-  byte[] fileBytes; // complete file in bytes
-  int currIdx = 0;
-  long fileSize;
-  long chunkSize;
-  long chunkCount;
-
-  Map<Integer, byte[]> bytesMap = new HashMap<>();
-  int bytesIdx = 0;
-
-  FileMetadata fileMetadata;
-
-  List<Path> listPath = new ArrayList<>();
-  List<FileOutputStream> listFos = new ArrayList<>();
-  int receiveFileCounter = 0;
-  int receiveChunkCounter = 0;
-  boolean isFinished;
 
   private ServerHandler serverHandler;
 
@@ -65,33 +36,7 @@ public class Client extends WebSocketClient {
 
   @Override
   public void onMessage(String message) {
-    if (message.startsWith("FILE-METADATA~")) {
-      try {
-        fos = new FileOutputStream(toBeReceived.toFile());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      String json = message.substring(message.indexOf('~') + 1);
-      ObjectMapper mapper = new ObjectMapper();
-      try {
-        this.fileMetadata = mapper.readValue(json, FileMetadata.class);
-        this.fileSize = this.fileMetadata.getFileSize();
-        this.chunkSize = this.fileMetadata.getChunkSize();
-        this.chunkCount = this.fileMetadata.getChunkCount();
-        // this.fileBytes = new byte[(int) this.fileSize];
-        this.readyToReceiveFile = true;
-        logger.info("RECEIVED META DATA");
-        logger.info("File size is : " + this.fileSize);
-        logger.info("chunk size is : " + this.chunkSize);
-        logger.info("chunk count is : " + this.chunkCount);
-        // logger.info("file bytes length is : " + this.fileBytes.length);
-        for (long i = 0; i < chunkCount; i++) {
-          send("CHUNK-ID~" + i);
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    } else if (message.startsWith("server/")) {
+    if (message.startsWith("server/")) {
       this.serverHandler.handleString(message.substring(7));
     }
   }
